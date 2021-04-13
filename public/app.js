fb68fb0bbcd1411098d82a704bd7190a372e7233
Kemal Aydık <kemalaydik@gmail.com>
2021-04-13 20:45:47 +0300

    interim

--- public/app.js
+++ public/app.js
@@ -1,1 +1,38 @@
-const inp=document.querySelector(inp)
+const inp = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const renderTodo = (e) => {
+	const li = document.createElement('li');
+	if (e.completed) li.classList.add('done');
+	li.innerHTML = e.name + '<span class="delete">X</span>';
+	ul.append(li);
+};
+const renderTodos = (response) => {
+	response.data.forEach(renderTodo);
+};
+
+axios.get('/api/todos').then(renderTodos);
+// .then(() => {
+// 	const listItems = document.querySelectorAll('li');
+// 	console.log([...listItems]);
+
+// 	[...listItems].forEach((e) => {
+// 		console.log(e);
+// 		e.addEventListener('click', () => e.classList.toggle('done'));
+// 	});
+// });
+
+inp.addEventListener('keypress', (e) => {
+	if (e.keyCode === 13) {
+		axios.post('/api/todos', { name: inp.value }).then((todo) => {
+			renderTodo(todo.data);
+			inp.value = '';
+		});
+	}
+});
+
+ul.addEventListener('click', (e) => {
+	if (e.target.className==='delete') {
+        console.log(this);
+    };
+});
--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express'),
     router=express.Router(),
     helpers=require('../helpers/todos');
 
-router.route('/',).get(helpers.getTodos).post(helpers.createTodo)
+router.route('/').get(helpers.getTodos).post(helpers.createTodo)
 router.route('/:todoId').get(helpers.getTodo).put(helpers.updateTodo).delete(helpers.deleteTodo)
 
 
